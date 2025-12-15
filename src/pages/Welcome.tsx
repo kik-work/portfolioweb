260476b3b171b90e1367a299fc5432d12b52ccd4
kik-work <kik.work100@gmail.com>
2025-12-15 16:57:28 +0600

    removed loading text

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -11,27 +11,27 @@ const SkillsPage = lazy(() => import("./Skills"));
 const Welcome = () => {
   return (
     <section className="max-w-7xl mx-auto px-2 rounded-2xl py-1 bg-slate-50/50 dark:bg-slate-900/50 backdrop-blur-md border border-slate-200/10">
-      <Suspense fallback={<div className="text-center py-10">Loading Home...</div>}>
+      <Suspense fallback={<div className="text-center py-10"></div>}>
         <HomePage />
       </Suspense>
 
-      <Suspense fallback={<div className="text-center py-10">Loading Experience...</div>}>
-        <ExperiencePage goToProjects={() => {}} />
+      <Suspense fallback={<div className="text-center py-10"></div>}>
+        <ExperiencePage  />
       </Suspense>
 
-      <Suspense fallback={<div className="text-center py-10">Loading Projects...</div>}>
+      <Suspense fallback={<div className="text-center py-10">=</div>}>
         <ProjectPage />
       </Suspense>
 
-      <Suspense fallback={<div className="text-center py-10">Loading Skills...</div>}>
+      <Suspense fallback={<div className="text-center py-10"></div>}>
         <SkillsPage />
       </Suspense>
 
-      <Suspense fallback={<div className="text-center py-10">Loading Education...</div>}>
+      <Suspense fallback={<div className="text-center py-10"></div>}>
         <EducationPage />
       </Suspense>
 
-      <Suspense fallback={<div className="text-center py-10">Loading About...</div>}>
+      <Suspense fallback={<div className="text-center py-10"></div>}>
         <AboutPage />
       </Suspense>
     </section>
